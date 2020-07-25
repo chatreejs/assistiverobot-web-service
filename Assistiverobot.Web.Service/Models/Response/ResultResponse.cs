5578cd9f22a3647016fa7d2c97879aa611f7048a
CHANON TREEMETH <chanon.t@cdg.co.th>
2020-07-25 23:13:26 +0700

    Edit response

--- Assistiverobot.Web.Service/Models/Response/ResultResponse.cs
+++ Assistiverobot.Web.Service/Models/Response/ResultResponse.cs
@@ -1,23 +1,26 @@
-using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
 using Assistiverobot.Web.Service.Constants;
-using Microsoft.AspNetCore.Diagnostics;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 
 namespace Assistiverobot.Web.Service.Models.Response
 {
     public class ResultResponse
     {
-        private string Message { get; set; }
-        private object Result { get; set; }
-        public static object GetResultSuccess(object result = null)
+        public string Message { get; set; }
+        public object Result { get; set; }
+        public static object GetResultSuccess(object result)
         {
             return new ResultResponse()
             {
                 Message = StatusMessage.MESSAGE_SUCCESS,
                 Result = result,
+            };
+        }
+
+        public static object GetResultInternalError()
+        {
+            return new ResultResponse()
+            {
+                Message = StatusMessage.MESSAGE_INTERNAL_ERROR,
+                Result = null
             };
         }
     }
