8017a105e190700821536efe8cd662bbbba3a229
CHANON TREEMETH <chanon.t@cdg.co.th>
2020-09-09 21:07:24 +0700

    Add status constants

---
+++ Assistiverobot.Web.Service/Constants/JobStatus.cs
@@ -0,0 +1,10 @@
+namespace AssistiveRobot.Web.Service.Constants
+{
+    public static class JobStatus
+    {
+        public const string StatusPending = "pending";
+        public const string StatusRunning = "running";
+        public const string StatusSuccess = "success";
+        public const string StatusFailed = "failed";
+    }
+}
